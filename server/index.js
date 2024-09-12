2f52b794c9f5c78eb0d6dac6753ba632b309054e
archisman saha <76175105+archismansaha@users.noreply.github.com>
2024-09-12 20:21:25 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,17 +8,10 @@ import dalleRoutes from './routes/dalleRoutes.js';
 dotenv.config();
 const whitelist = ['https://genetiv-delta.vercel.app/']; //white list consumers
 const corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(null, false);
-        }
-    },
-    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-    credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  "origin": "*",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
 };
 const app = express();
 app.use(cors(corsOptions));
