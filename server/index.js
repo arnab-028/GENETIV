4058b13128142cdb870897754cab28de27557e41
archisman saha <76175105+archismansaha@users.noreply.github.com>
2024-09-12 20:36:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
-})
+});
 app.use(express.json({ limit: '50mb' })); // Limit request body size to 50MB
 
 //building the APi endpoints
